a5111193fca257d36e17848722a0e547b2ce04ea
Matteo Fava <matteo.luca.fava@gmail.com>
2025-09-28 21:58:07 +0200

    sim con energie

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use newt::sim::simulation::Simulation;
 
 fn main() -> anyhow::Result<()> {
-    let mut sim = Simulation::from_config_file("src/examples/test.json")?;
+    let p = (-10000., 10000.);
+    let mut sim = Simulation::random_system(1., 0.001, 1000, 1000, 0.01, (1., 1.), [p; 3], None);
+    let e0 = sim.total_e();
+    println!("Total E = {}", e0);
     sim.run();
+    let e = sim.total_e();
+    println!("Total E = {}", e);
 
     Ok(())
 }
--- src/sim/simulation.rs
+++ src/sim/simulation.rs
@@ -3,6 +3,7 @@ use crate::sim::{
     geom::Vec3,
 };
 use anyhow::Result;
+use rand::{rng, Rng};
 use std::mem::swap;
 
 #[derive(Debug, PartialEq)]
@@ -32,6 +33,62 @@ impl Simulation {
     pub fn from_config_file(path: &str) -> anyhow::Result<Self> {
         let conf = read_config(path)?;
         Ok(Self::from_config(conf))
+    }
+
+    pub fn random_system(
+        g: f64,
+        dt: f64,
+        steps: i64,
+        n: usize,
+        softening: f64,
+        mass_r: (f64, f64),
+        pos_r: [(f64, f64); 3],
+        vel_r: Option<[(f64, f64); 3]>,
+    ) -> Self {
+        let mut rng = rng();
+        let mut pos = vec![Vec3::zero(); n];
+        let mut mass = vec![0.0; n];
+        let mut vel = vec![Vec3::zero(); n];
+
+        let (mi, mf) = mass_r;
+        let [(xi, xf), (yi, yf), (zi, zf)] = pos_r;
+        let vel_ranges = vel_r.unwrap_or([(0., 0.); 3]);
+        let [(vxi, vxf), (vyi, vyf), (vzi, vzf)] = vel_ranges;
+
+        for i in 0..n {
+            // massa
+            mass[i] = if mi != mf {
+                rng.random_range(mi..mf)
+            } else {
+                mi
+            };
+
+            pos[i] = Vec3::from([
+                rng.random_range(xi..xf),
+                rng.random_range(yi..yf),
+                rng.random_range(zi..zf),
+            ]);
+
+            vel[i] = if vel_r.is_some() {
+                Vec3::from([
+                    rng.random_range(vxi..vxf),
+                    rng.random_range(vyi..vyf),
+                    rng.random_range(vzi..vzf),
+                ])
+            } else {
+                Vec3::zero()
+            };
+        }
+
+        Self {
+            g,
+            dt,
+            steps,
+            softening,
+            mass,
+            pos,
+            vel,
+        }
     }
 
     pub fn save_to_file(&self, path: &str) -> Result<()> {
@@ -91,6 +148,22 @@ impl Simulation {
             k += 0.5 * self.mass[i] * self.vel[i].abs2();
         }
         k
+    }
+
+    pub fn total_potential(&self) -> f64 {
+        let mut v = 0.;
+        let e = self.softening * self.softening;
+        for i in 0..self.pos.len() - 1 {
+            for j in i + 1..self.pos.len() {
+                let r = ((self.pos[i] - self.pos[j]).abs2() + e).sqrt();
+                v += self.mass[i] * self.mass[j] / r;
+            }
+        }
+        -self.g * v
+    }
+
+    pub fn total_e(&self) -> f64 {
+        self.total_kinetic() + self.total_potential()
     }
 
     //Run simulation with velocity verlet for n steps
