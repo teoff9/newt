caf43dc9fb748b255f09b7acd8bac778fc4e3460
Matteo Fava <matteo.luca.fava@gmail.com>
2025-09-29 20:14:42 +0200

    before reduction

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use newt::sim::simulation::Simulation;
 
 fn main() -> anyhow::Result<()> {
-    let p = (-10000., 10000.);
-    let mut sim = Simulation::random_system(1., 0.001, 1000, 1000, 0.01, (1., 1.), [p; 3], None);
+    let p = (-500., 500.);
+    let mut sim =
+        Simulation::random_system(1., 0.001, 10000, 100, 0.00001, (1., 1.2), [p; 3], None);
     let e0 = sim.total_e();
     println!("Total E = {}", e0);
+    let ti = std::time::Instant::now();
     sim.run();
+    let t = ti.elapsed().as_secs_f64();
     let e = sim.total_e();
     println!("Total E = {}", e);
+    println!("Time: {}", t);
 
     Ok(())
 }
