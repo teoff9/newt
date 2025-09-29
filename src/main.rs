e083dcdbe657c9e9ca133284f421d166ad29b2fd
Matteo Fava <matteo.luca.fava@gmail.com>
2025-09-29 21:18:52 +0200

    pre optim

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use newt::sim::simulation::Simulation;
 
 fn main() -> anyhow::Result<()> {
-    let p = (-500., 500.);
-    let mut sim =
-        Simulation::random_system(1., 0.001, 10000, 100, 0.00001, (1., 1.2), [p; 3], None);
+    // let p = (-500., 500.);
+    // let mut sim =
+    //     Simulation::random_system(1., 0.001, 10000, 100, 0.00001, (1., 1.2), [p; 3], None);
+    let mut sim = Simulation::from_config_file("src/examples/run1.json")?;
     let e0 = sim.total_e();
     println!("Total E = {}", e0);
     let ti = std::time::Instant::now();
